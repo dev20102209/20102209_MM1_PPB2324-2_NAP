bdb28ea61dd2b4ae123a80df3dedd5e671150357
dev20102209 <rizkynugroho2901@gmail.com>
2024-06-17 17:45:41 +0700

    commit praktikum 4

---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+ runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ const MyApp({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     title: 'Praktikum 4',
+     home: Scaffold(
+       appBar: AppBar(
+         leading: const Icon(Icons.arrow_back),
+         backgroundColor: Colors.blue,
+       ),
+       body: ListView(
+         children: [
+           //column pertama
+           Container(
+             height: 250,
+             color: Colors.blue,
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 //image
+                 Container(
+                   padding: const EdgeInsets.all(20)
+                   ,decoration: const BoxDecoration(
+                     shape: BoxShape.circle,
+                     color: Colors.lightBlueAccent
+                   ),
+                   child: const Icon(Icons.person, size: 30,),
+                 ),
+                 const SizedBox(height: 15,),
+
+                 //Nama
+                 const Text ("Yusron Rizky Putra", style: TextStyle(
+                   color: Colors.white,
+                   fontWeight: FontWeight.bold
+                 ),),
+                 const SizedBox(height: 5,),
+                 //NIM
+                 const Text ("20102209",style: TextStyle(
+                     color: Colors.white,
+                     fontWeight: FontWeight.bold
+                 ),),
+                 const SizedBox(height: 5,),
+                 const Text ("Mahasiswa",style: TextStyle(
+                     color: Colors.white,
+                 ),)
+               ],
+             ),
+           ),
+
+           //column kedua
+           Container(
+             margin: const EdgeInsets.all(30),
+             padding: const EdgeInsets.all(20),
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(10),
+               color: const Color(0xffFEF4F3)
+             ),
+             child: const Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text("Data Diri", style: TextStyle(
+                   color: Colors.pinkAccent,
+                   fontWeight: FontWeight.bold
+                 ),),
+                 SizedBox(height: 15,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     Text("Nama "),
+                     Text("Yusron")
+                   ],
+                 ),
+                 SizedBox(height: 15,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     Text("Kelas "),
+                     Text("IF")
+                   ],
+                 ),
+                 SizedBox(height: 15,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     Text("Program Studi"),
+                     Text("Teknik Informatika")
+                   ],
+                 ),
+                 SizedBox(height: 15,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     Text("Dosen Wali "),
+                     Text("NAP")
+                   ],
+                 ),
+                 SizedBox(height: 15,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     Text("Angkatan "),
+                     Text("2020")
+                   ],
+                 )
+               ],
+
+             ),
+           ),
+
+           //column ketiga
+           Container(
+             margin: const EdgeInsets.all(30),
+             padding: const EdgeInsets.all(20),
+             decoration: BoxDecoration(
+                 borderRadius: BorderRadius.circular(10),
+                 color: const Color(0xffFEF4F3)
+             ),
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 const Text("Pusat Bantuan", style: TextStyle(
+                   fontWeight: FontWeight.bold,
+                   color: Colors.pinkAccent
+                 ),),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     const Text("Bantuan"),
+                     Image.asset("assets/images/bantuan.png", scale: 5)
+
+                   ],
+                 ),
+                 const SizedBox(height: 15,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     const Text("Laporkan Masalah"),
+                     Image.asset("assets/images/report_icon.png",scale: 15)
+
+                   ],
+                 ),
+               ],
+             ),
+            
+           )
+
+         ],
+       ),
+     )
+   );
+ }
+}
